b075e942e3f0896896572d09428cfc213b9f37f8
DELL <sachinialahakoon327@gmail.com>
2023-05-10 21:52:38 +0530

    Exam Marks Checking Program

---
+++ main.py
@@ -0,0 +1,17 @@
+x = 0
+x = int(input("Enter the marks: "))
+if x >= 75:
+    print("A")
+elif x >=65:
+    print("B")
+elif x >=55:
+    print("C")
+elif x >=35:
+    print("S")
+else:
+    print("F")
+
+
+
+
+
